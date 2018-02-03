42c59f6d003f0b4be9351a1afc613406a45fb663
Trevyn Mace <tmace@1800contacts.com>
2018-02-03 14:40:57 -0800

    adding new component

---
+++ ClientApp/src/components/DayList.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react'
+import { CardGroup } from 'reactstrap'
+
+export class DayList extends Component {
+  constructor() {
+    super()
+  }
+
+  render () {
+    const days = [1, 2, 3, 4, 5]
+
+    return (
+      <div>
+        <CardGroup>
+          {days.map((day) => {
+            return <div>{day}</div>
+          })}
+        </CardGroup>
+      </div>
+    )
+  }
+}
